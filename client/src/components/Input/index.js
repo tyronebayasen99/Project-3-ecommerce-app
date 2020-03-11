6112717b178e01d1da5950e32ec9be5a17ea94b3
Rachels Mac <rasilverthorn@ucdavis.edu>
2020-03-10 20:37:35 -0700

    add

--- client/src/components/Input/index.js
+++ client/src/components/Input/index.js
@@ -1,12 +1,16 @@
 import React from "react";
+import "./style.css";
+
 
 // This component lets us use a bootstrap input element without worrying about class names
 // or manually wrapping the input with a form-group div
 // All of the props passed to this component are spread onto the input element
 function Input() {
     return (
-        <div className="input-group input-group-lg">
-            <input value="input" className="form-control" type="text" />
+        <div className="input-group input-group-lg" id="inputPrice">
+            <input value="Price Range" className="form-control" type="text" />
+
+
         </div>
     );
 }

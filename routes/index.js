1b7ec8f9493bc07919c62df76e7c9be39bf6db00
tyronebayasen99 <tyronejbayasen@gmail.com>
2020-03-19 12:00:59 -0700

    working map results

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
+const path = require("path");
 const app = require("express").Router();
 const apiRoutes = require("./api");
 
 app.use("/api", apiRoutes);
+
+// If no API routes are hit, send the React app
+app.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 module.exports = app;

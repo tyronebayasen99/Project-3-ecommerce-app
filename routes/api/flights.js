26f2c007f8931068b313adfbe4d491ff87679978
tyronebayasen99 <tyronejbayasen@gmail.com>
2020-03-19 12:04:51 -0700

    working mapping search results

--- routes/api/flights.js
+++ routes/api/flights.js
@@ -5,4 +5,8 @@ app.route("/save/:id").post(flightController.saveFlight);
 
 app.route("/search/:depart-:arrival").get(flightController.searchFlight);
 
+// app.get("/search/:depart-:arrival", function(req, res) {
+//   res.send(flightController.searchFlight);
+// });
+
 module.exports = app;
